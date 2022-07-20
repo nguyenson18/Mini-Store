768d777e68468e626008e6bddfd6c6779212878c
nguyenson18 <nguyentruongsonpp@.com>
2022-07-20 15:05:54 +0700

    up file

--- src/contexts/AuthContext.js
+++ src/contexts/AuthContext.js
@@ -11,7 +11,7 @@ const INITIALIZATION = "INITIALIZATION";
 const LOGIN_SUCCESS = "LOGIN_SUCCESS";
 const LOGOUT = "LOGOUT";
 const REGISTER = "REGISTER";
-const LOGIN_ERROR = "LOGIN_ERROR"
+const LOGIN_ERROR = "LOGIN_ERROR";
 
 const reducer = (state, action) => {
   switch (action.type) {
